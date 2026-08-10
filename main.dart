18f3e3e61257ff824b86729049326440c23f08d2
isaacJ212 <isaacejimenez2006@gmail.com>
2026-08-10 16:03:35 -0600

    creacion de menu version finalv1

--- main.dart
+++ main.dart
@@ -1,17 +1,49 @@
+import 'dart:io';
+
 import 'class.dart';
 
 void main() {
-  final dog = Dog('Max', 3, 25.0, 'Labrador');
-  final cat = Cat('Mina', 2, 4.5, 'Negra');
-  final bird = Bird('Piolin', 1, 0.8, 'Canario');
 
-  dog.showInfo();
-  dog.makeSound();
-  dog.eat();
 
-  cat.showInfo();
-  cat.makeSound();
+  print("Selecina una opcion diponibe: ");
 
-  bird.showInfo();
-  bird.makeSound();
+  print("1. Perro");
+  print("2. Gato");
+  print("3. Pajaro"); 
+  print("4. Salir");
+  int opcion = int.parse(stdin.readLineSync()!);
+
+  do {
+    switch (opcion) {
+      case 1:
+        Dog dog = Dog('Dog', 3, 10.5, 'Labrador');
+        dog.showInfo();
+        dog.makeSound();
+        break;
+      case 2:
+        Cat cat = Cat('Cat', 2, 5.0, 'Black');
+        cat.showInfo();
+        cat.makeSound();
+        break;
+      case 3:
+        Bird bird = Bird('Bird', 1, 0.5, 'Parrot');
+        bird.showInfo();
+        bird.makeSound();
+        break;
+      case 4:
+        print("Saliendo del programa...");
+        break;
+      default:
+        print("Opcion invalida. Intente nuevamente.");
+    }
+
+    print("\nSeleccione una opcion disponible: ");
+    print("1. Perro");
+    print("2. Gato");
+    print("3. Pajaro"); 
+    print("4. Salir");
+    opcion = int.parse(stdin.readLineSync()!);
+    
+  } while (opcion != 4);
+
 }
