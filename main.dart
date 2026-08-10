4ae37dcb8bde139cd125bf4ef04b6ca68fa9d072
Fabian <fabianarte121@gmail.com>
2026-08-10 16:16:55 -0600

    final final

--- main.dart
+++ main.dart
@@ -3,47 +3,94 @@ import 'dart:io';
 import 'class.dart';
 
 void main() {
-
-
-  print("Selecina una opcion diponibe: ");
-
-  print("1. Perro");
-  print("2. Gato");
-  print("3. Pajaro"); 
-  print("4. Salir");
-  int opcion = int.parse(stdin.readLineSync()!);
+  int opcion;
 
   do {
-    switch (opcion) {
-      case 1:
-        Dog dog = Dog('Dog', 3, 10.5, 'Labrador');
-        dog.showInfo();
-        dog.makeSound();
-        break;
-      case 2:
-        Cat cat = Cat('Cat', 2, 5.0, 'Black');
-        cat.showInfo();
-        cat.makeSound();
-        break;
-      case 3:
-        Bird bird = Bird('Bird', 1, 0.5, 'Parrot');
-        bird.showInfo();
-        bird.makeSound();
-        break;
-      case 4:
-        print("Saliendo del programa...");
-        break;
-      default:
-        print("Opcion invalida. Intente nuevamente.");
-    }
-
-    print("\nSeleccione una opcion disponible: ");
+    print("\nSeleccione una opcion disponible:");
     print("1. Perro");
     print("2. Gato");
-    print("3. Pajaro"); 
+    print("3. Pajaro");
     print("4. Salir");
-    opcion = int.parse(stdin.readLineSync()!);
-    
-  } while (opcion != 4);
 
+    stdout.write("Opcion: ");
+    opcion = int.parse(stdin.readLineSync()!);
+
+    switch (opcion) {
+      case 1:
+        print("\n--- Registrar Perro ---");
+
+        stdout.write("Nombre: ");
+        String name = stdin.readLineSync()!;
+
+        stdout.write("Edad: ");
+        int age = int.parse(stdin.readLineSync()!);
+
+        stdout.write("Peso: ");
+        double weight = double.parse(stdin.readLineSync()!);
+
+        stdout.write("Raza: ");
+        String breed = stdin.readLineSync()!;
+
+        Dog dog = Dog(name, age, weight, breed);
+
+        print("\nInformacion del perro:");
+        dog.showInfo();
+        dog.makeSound();
+
+        break;
+
+      case 2:
+        print("\n--- Registrar Gato ---");
+
+        stdout.write("Nombre: ");
+        String name = stdin.readLineSync()!;
+
+        stdout.write("Edad: ");
+        int age = int.parse(stdin.readLineSync()!);
+
+        stdout.write("Peso: ");
+        double weight = double.parse(stdin.readLineSync()!);
+
+        stdout.write("Color: ");
+        String color = stdin.readLineSync()!;
+
+        Cat cat = Cat(name, age, weight, color);
+
+        print("\nInformacion del gato:");
+        cat.showInfo();
+        cat.makeSound();
+
+        break;
+
+      case 3:
+        print("\n--- Registrar Pajaro ---");
+
+        stdout.write("Nombre: ");
+        String name = stdin.readLineSync()!;
+
+        stdout.write("Edad: ");
+        int age = int.parse(stdin.readLineSync()!);
+
+        stdout.write("Peso: ");
+        double weight = double.parse(stdin.readLineSync()!);
+
+        stdout.write("Especie: ");
+        String species = stdin.readLineSync()!;
+
+        Bird bird = Bird(name, age, weight, species);
+
+        print("\nInformacion del pajaro:");
+        bird.showInfo();
+        bird.makeSound();
+
+        break;
+
+      case 4:
+        print("\nSaliendo del programa...");
+        break;
+
+      default:
+        print("\nOpcion invalida. Intente nuevamente.");
+    }
+  } while (opcion != 4);
 }
